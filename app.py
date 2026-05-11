b92e8cbdb3c2e7c0e7eea048d6d1c273477c0214
Taekyon <lean.asai@eduvaud.ch>
2026-05-11 11:45:16 +0200

    modif message 01

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "Hello EPSIC - DevOps - ALED"
+    return "Hello EPSIC - DevOps - Modifié par Jenskins !"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
